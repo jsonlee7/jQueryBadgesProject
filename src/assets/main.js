4e363f784c695b94ea03c8e05cfee3110ac806fb
Jason Lee <jlee@fcdjfs.co.franklin.oh.us>
2017-01-03 11:35:10 -0500

    added code to assign ajax response to dom elements

--- src/assets/main.js
+++ src/assets/main.js
@@ -3,8 +3,30 @@ var url = 'https://codeschool.pluralsight.com/users/2136740.json';
   $.ajax({
     url:url,
     dataType: 'jsonp',
-    success: function(){
-      console.log('successfully retrieved report card');
+    success: function(response){
+      var badges = $('#badges');
+      response.courses.completed.forEach(function(course){
+        console.log('course',course);
+      var $course =  $('<div />', {
+          'class' : 'course'
+        }).appendTo(badges);
+        
+      $('<h3 />', {
+        text: course.title
+      }).appendTo($course);
+      
+      $('<img />', {
+        src: course.badge
+      }).appendTo($course);
+      
+      $('<a />', {
+        href: course.url,
+        target: '_blank',
+        class: 'btn btn-primary',
+        text: 'See Course'
+      }).appendTo($course);
+        
+      });            
     }
   })
 
